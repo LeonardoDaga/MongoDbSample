ff1a00f9634c80d45c49623eb5c8f4bbbeda63be
Leonardo Daga <leonardo.daga@gmail.com>
2020-03-23 00:40:53 +0100

    Added Bulk write sample

---
+++ MongoDbSample/MongoDbConsoleBulkWrite/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Diagnostics;
+
+namespace MongoDbConsoleBasic
+{
+    public class User
+    {
+        // The unique user identity - Mongo can provide a unique Id but if you're linking back to SQL stick to an in
+        [BsonId]
+        public int UserId;
+
+        // The users position data as a nested object, see class below for details
+        public List<PositionItem> UserPositions;
+
+        public override string ToString()
+        {
+            string res = string.Format("UserId = {0}\n", UserId);
+            res += string.Format("UserPositions (count = {0})\n", UserPositions.Count);
+
+            foreach (var item in UserPositions)
+            {
+                res += item.ToString();
+            }
+
+            return res;
+        }
+    }
+
+    // Supporting/Partial Class for position data
+    public class PositionItem
+    {
+        // The date time for this position
+        public int PositionDate;
+
+        // Doesn't have to be a string, but basically whatever data identifies the position
+        public string Position;
+
+        public override string ToString()
+        {
+            string res = string.Format("PositionItem: Date = {0}, Position = {1}\n", PositionDate, Position);
+            return res;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // get a mongoclient using the default connection string
+            var mongo = new MongoClient();
+
+            // get (and create if doesn't exist) a database from the mongoclient
+            var db = mongo.GetDatabase("UserPositionsDb");
+
+            // get a collection of User (and create if it doesn't exist)
+            var collection = db.GetCollection<User>("UserCollection");
+
+            // Random positions generator
+            Random rand = new Random(DateTime.Now.Second);
+
+            Stopwatch sw = new Stopwatch();
+
+
+            // Increase the day number to simulate the daily update for many users
+            // to check how the record addition slows the db when the records number grows
+            for (int day = 0; day < 100; day++)
+            {
+                List<WriteModel<User>> _listBulkUserWrites = new List<WriteModel<User>>();
+
+                sw.Reset();
+                sw.Start();
+
+                // Loop for 16*12 users data update
+                for (int userID = 1000; userID < 1000 + 16 * 12; userID++)
+                {
+                    // Add the new record to the document
+                    var positionItem = new PositionItem
+                    {
+                        Position = (41 + rand.NextDouble() * 2).ToString() + "," + (11 + rand.NextDouble() * 2).ToString(),
+                        PositionDate = day
+                    };
+
+                    var updateUserFilter = Builders<User>.Filter.Eq(u => u.UserId, userID);
+                    var updateUserDefinition = Builders<User>.Update.Push(u => u.UserPositions, positionItem);
+
+                    _listBulkUserWrites.Add(new UpdateOneModel<User>(updateUserFilter,
+                                updateUserDefinition)
+                                { IsUpsert = true }
+                    );
+                }
+
+                if (_listBulkUserWrites.Count > 0)
+                    collection.BulkWriteAsync(_listBulkUserWrites);
+
+                sw.Stop();
+
+                Console.WriteLine("Elapsed Time = {0}ms", sw.Elapsed.TotalMilliseconds);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # MongoDbSample
 My first example using MongoDb to create an archive containing a collection of documents, each document containing a list of information and the time it has been collected.
 
-2 Projects are available:
+5 Projects are available:
 - MongoDbConsoleBasic: A read/write single test, to verify the functionality
 - MongoDbConsoleEfficiencyTestBasic: A multiple write test, to verify the efficiency of the Retrieve/ReplaceOne instructions;
 - MongoDbConsoleUpsert: A single write test, to verify the functionality of the UpdateOne (Upsert) instruction
 - MongoDbConsoleEfficiencyTestUpsert: A multiple write test, to verify the efficiency of the Update One (Upsert) instructions;
+- MongoDbConsoleBulkWrite: A multiple write test, to verify the efficiency of the Bulk write. 
 
 In the MongoDbConsoleEfficiencyTestUpsert test I added in the repository cited above I’ve found that the Upsert time is 3 to 5 (depending of the size of the array) faster than the two round trips (MongoDbConsoleEfficiencyTestBasic) approach.
+Bulk write demonstrated in MongoDbConsoleBulkWrite is even faster, showing an average time more than 10 times better than the MongoDbConsoleEfficiencyTestUpsert example.
+
+Read MauroPetrini (https://dev.to/mpetrinidev/a-guide-to-bulk-write-operations-in-mongodb-with-c-51fk) for information on a faster approach to write (bulk write) a set of field in a burst way.
